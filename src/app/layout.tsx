12d7415bd9c780c04fcd4962721b57beacde551f
Ekjot Singh (Joe) <ekjot.singh@bookdepot.com>
2026-02-27 18:48:11 -0500

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,19 @@
-
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-// Remove the import completely
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "NZ Moving Services | Affordable Christchurch Movers",
   description: "Your locally owned moving company in Christchurch...",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
